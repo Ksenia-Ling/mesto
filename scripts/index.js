7c13eff1742d1ab06c291542b7403b3e4fb16d62
Ksenia Kozyreva <oestara91@gmail.com>
2022-04-26 21:29:42 +0300

    Изменена структура скрипта, внесены правки в js по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const content = document.querySelector('.content');
-const editProfileButton = content.querySelector('.profile__edit-button');
+const profileEditButton = content.querySelector('.profile__edit-button');
 const popupProfileWindow = document.querySelector('.popup_profile');
 const popupProfileClose = popupProfileWindow.querySelector('.popup__close-button');
 
@@ -9,57 +9,36 @@ const jobInput = userDataForm.querySelector('.popup__input_job');
 const userName = content.querySelector('.profile__heading');
 const userJob = content.querySelector('.profile__subheading');
 
-function openPopupProfileWindow() {
-  popupProfileWindow.classList.toggle('popup_opened');
-  nameInput.value = userName.textContent;
-  jobInput.value = userJob.textContent;
+const cardsContainer = document.querySelector('.cards');
+const templateCards = document.querySelector('.templateCards');
+
+const popupImageZoom = document.querySelector('.popup_image-zoom');
+const popupImageLink = popupImageZoom.querySelector('.popup__image-link');
+const popupImageCaption = popupImageZoom.querySelector('.popup__image-caption');
+const popupImageZoomClose = popupImageZoom.querySelector('.popup__close-button')
+
+const placeAddingButton = content.querySelector('.profile__add-button');
+const popupPlaceWindow = document.querySelector('.popup_new-place');
+const popupPlaceClose = popupPlaceWindow.querySelector('.popup__close-button');
+
+const newPlaceForm = popupPlaceWindow.querySelector('.popup__input-container');
+const titleInput = newPlaceForm.querySelector('.popup__input_title');
+const linkInput = newPlaceForm.querySelector('.popup__input_link');
+
+function openPopupWindow(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function closePopupProfileWindow() {
-  popupProfileWindow.classList.toggle('popup_opened');
+function closePopupWindow(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-editProfileButton.addEventListener('click', openPopupProfileWindow);
-popupProfileClose.addEventListener('click', closePopupProfileWindow);
-
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
-  closePopupProfileWindow();
+  closePopupWindow(popupProfileWindow);
 }
-
-userDataForm.addEventListener('submit', formSubmitHandler);
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const cardsContainer = document.querySelector('.cards');
-const templateCards = document.querySelector('.templateCards');
 
 function addCards() {
   const htmlCards = initialCards.map(getElement);
@@ -91,45 +70,45 @@ function getElement(card) {
   return getTemplateCard;
 }
 
-const popupImageZoom = document.querySelector('.popup_image-zoom');
-let popupImageLink = popupImageZoom.querySelector('.popup__image-link');
-let popupImageCaption = popupImageZoom.querySelector('.popup__image-caption');
-const popupImageZoomClose = popupImageZoom.querySelector('.popup__close-button')
-
-function togglePopupImageWindow() {
-  popupImageZoom.classList.toggle('popup_opened');
-}
-
 function zoomImagePopup(item) {
-  togglePopupImageWindow();
+  openPopupWindow(popupImageZoom);
   popupImageLink.src = item.link;
+  popupImageLink.alt = item.name;
   popupImageCaption.textContent = item.name;
 }
 
-popupImageZoomClose.addEventListener('click', togglePopupImageWindow);
-
-const addPlaceButton = content.querySelector('.profile__add-button');
-const popupPlaceWindow = document.querySelector('.popup_new-place');
-const popupPlaceClose = popupPlaceWindow.querySelector('.popup__close-button');
-
-const newPlaceForm = popupPlaceWindow.querySelector('.popup__input-container');
-const titleInput = newPlaceForm.querySelector('.popup__input_title');
-const linkInput = newPlaceForm.querySelector('.popup__input_link');
-
-
-function togglePopupPlaceWindow() {
-  popupPlaceWindow.classList.toggle('popup_opened');
-}
-
-addPlaceButton.addEventListener('click', togglePopupPlaceWindow);
-popupPlaceClose.addEventListener('click', togglePopupPlaceWindow);
-
-function placeFormSubmitHandler(evt) {
+function handlePlaceFormSubmit(evt) {
   evt.preventDefault();
   const newCard = getElement({ name: titleInput.value, link: linkInput.value });
   cardsContainer.prepend(newCard);
-  togglePopupPlaceWindow();
+  closePopupWindow(popupPlaceWindow);
+  titleInput.value = "";
+  linkInput.value = "";
 }
 
-newPlaceForm.addEventListener('submit', placeFormSubmitHandler);
+profileEditButton.addEventListener('click', function () {
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
+  openPopupWindow(popupProfileWindow);
+});
+
+popupProfileClose.addEventListener('click', () => closePopupWindow(popupProfileWindow));
+
+userDataForm.addEventListener('submit', handleProfileFormSubmit);
+
+popupImageZoomClose.addEventListener('click', () => closePopupWindow(popupImageZoom));
+
+placeAddingButton.addEventListener('click', () => openPopupWindow(popupPlaceWindow));
+popupPlaceClose.addEventListener('click', () => closePopupWindow(popupPlaceWindow));
+
+newPlaceForm.addEventListener('submit', handlePlaceFormSubmit);
+
+
+
+
+
+
+
+
+
 
