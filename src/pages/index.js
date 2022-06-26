95959fe1e55841ef1a7168ffe844f57d46183ab3
Ksenia Kozyreva <oestara91@gmail.com>
2022-06-26 06:25:09 +0300

    убран catch в классе Api, выполнены правки по ревью

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,6 +118,9 @@ const placeForm = new PopupWithForm({
       .catch((err) => {
         console.log(err)
       })
+      .finally(() => {
+        placeForm.returnInitialBtnText()
+      })
   }
 });
 
